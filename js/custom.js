09791309cd4e22715cb289dad314b6edee4415a5
pawel.idziak <208766@student.pwr.edu.pl>
2017-03-25 20:55:25 +0100

    New structure (bootstrap/less and js) + changes file import, add jQuery, add Google Fonts, change carousel interval

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+/**
+ * Created by pawel.idziak on 25.03.2017.
+ */
+
+$(document).ready(function () {
+
+    // top slider
+    $('#top-slider').carousel({
+        interval: 4000
+    })
+
+    $('#promo-slider').carousel({
+        interval: 3000
+    })
+})
